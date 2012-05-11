5f49b4537ff91be8e652f6ba031539dceb46a600
Tom de Simone <mail@tomandpete.co.uk>
2012-05-11 21:56:05 +0100

    increase indentation from 2 spaces to 4

--- public/static/js/chatroom.js
+++ public/static/js/chatroom.js
@@ -1,110 +1,110 @@
 var scrollBox = function ($elem) {
-  var height = $elem[0].scrollHeight;
-  $elem.scrollTop(height);
+    var height = $elem[0].scrollHeight;
+    $elem.scrollTop(height);
 };
 
 var addUser = function (id, username) {
-  $userLi = $('<li></li>');
-  $userLi.text(username);
-  $userLi.attr('data-user-id', id);
-  $('#users ul').append($userLi);
+    $userLi = $('<li></li>');
+    $userLi.text(username);
+    $userLi.attr('data-user-id', id);
+    $('#users ul').append($userLi);
 }
 
 $(document).ready(function () {
-  var lastLoad = new Date().getTime();
-  var socket = io.connect();
-  
-  socket.on('reload', function (data) {
-    window.location.reload(true);
-  });
-  
-  socket.on('server-full', function (data) {
-    $('#username-submit-result').text('');
-    $('#username-submit-result').removeClass('success').addClass('fail');
-    $('#username-submit-result').text('Server full; please try again later');
-  });
-  
-  socket.on('welcome', function (data) {
-    $('#username-submit-result').text('');
-    socket.emit('join', {username: $.cookie('chatroom-username'), lastLoad: lastLoad});
-  });
-  
-  socket.on('userlist', function (data) {
-    $('#users ul').children().remove();
-    for (i in data.users) {
-      addUser(i, data.users[i]);
-    }
-  });
-  
-  socket.on('message', function (data) {
-    var $div = $('<div></div>');
-    var $userSpan = $('<span class="username"></span>');
-    $userSpan.text('<' + data.username + '>');
-    var $msgSpan = $('<span class="message"></span>');
-    $msgSpan.text(data.text);
-    $div.append($userSpan);
-    $div.append(' ');
-    $div.append($msgSpan);
-    $('#messages').append($div);
+    var lastLoad = new Date().getTime();
+    var socket = io.connect();
     
-    scrollBox($('#messages'));
-  });
-  
-  socket.on('joined', function (data) {
-    var $messageDiv = $('<div class="notice notice-joined"></div>');
-    $messageDiv.text('* ' + data.username + ' joined');
-    $('#messages').append($messageDiv);
-    scrollBox($('#messages'));
+    socket.on('reload', function (data) {
+        window.location.reload(true);
+    });
     
-    if (data.self) {
-      $('#field-username').val(data.username);
-      $.cookie('chatroom-username', data.username, {expires: 365, path: '/'});
-    } else {
-      addUser(data.id, data.username);
-    }
-  });
-  
-  socket.on('left', function (data) {
-    var $messageDiv = $('<div class="notice notice-left"></div>');
-    $messageDiv.text('* ' + data.username + ' left');
-    $('#messages').append($messageDiv);
-    scrollBox($('#messages'));
+    socket.on('server-full', function (data) {
+        $('#username-submit-result').text('');
+        $('#username-submit-result').removeClass('success').addClass('fail');
+        $('#username-submit-result').text('Server full; please try again later');
+    });
+    
+    socket.on('welcome', function (data) {
+        $('#username-submit-result').text('');
+        socket.emit('join', {username: $.cookie('chatroom-username'), lastLoad: lastLoad});
+    });
+    
+    socket.on('userlist', function (data) {
+        $('#users ul').children().remove();
+        for (i in data.users) {
+            addUser(i, data.users[i]);
+        }
+    });
+    
+    socket.on('message', function (data) {
+        var $div = $('<div></div>');
+        var $userSpan = $('<span class="username"></span>');
+        $userSpan.text('<' + data.username + '>');
+        var $msgSpan = $('<span class="message"></span>');
+        $msgSpan.text(data.text);
+        $div.append($userSpan);
+        $div.append(' ');
+        $div.append($msgSpan);
+        $('#messages').append($div);
+        
+        scrollBox($('#messages'));
+    });
+    
+    socket.on('joined', function (data) {
+        var $messageDiv = $('<div class="notice notice-joined"></div>');
+        $messageDiv.text('* ' + data.username + ' joined');
+        $('#messages').append($messageDiv);
+        scrollBox($('#messages'));
+        
+        if (data.self) {
+            $('#field-username').val(data.username);
+            $.cookie('chatroom-username', data.username, {expires: 365, path: '/'});
+        } else {
+            addUser(data.id, data.username);
+        }
+    });
+    
+    socket.on('left', function (data) {
+        var $messageDiv = $('<div class="notice notice-left"></div>');
+        $messageDiv.text('* ' + data.username + ' left');
+        $('#messages').append($messageDiv);
+        scrollBox($('#messages'));
 
-    $('#users li[data-user-id=' + data.id + ']').remove();
-  });
-  
-  socket.on('changed-username', function (data) {
-    var $messageDiv = $('<div class="notice notice-changed-username"></div>');
-    $messageDiv.text('* ' + data.oldName + ' is now known as ' + data.newName);
-    $('#messages').append($messageDiv);
-    scrollBox($('#messages'));
+        $('#users li[data-user-id=' + data.id + ']').remove();
+    });
     
-    var $userLi = $('#users li[data-user-id=' + data.id + ']');
-    $userLi.text(data.newName);
-  });
-  
-  socket.on('change-username-submit', function (data) {
-    $('#username-submit-result').text('');
-    if (data.success) {
-      $('#username-submit-result').removeClass('fail').addClass('success');
-      $.cookie('chatroom-username', data.username, {expires: 365, path: '/'});
-    } else {
-      $('#username-submit-result').removeClass('success').addClass('fail');
-    }
-    $('#username-submit-result').text(data.message);
-  });
+    socket.on('changed-username', function (data) {
+        var $messageDiv = $('<div class="notice notice-changed-username"></div>');
+        $messageDiv.text('* ' + data.oldName + ' is now known as ' + data.newName);
+        $('#messages').append($messageDiv);
+        scrollBox($('#messages'));
+        
+        var $userLi = $('#users li[data-user-id=' + data.id + ']');
+        $userLi.text(data.newName);
+    });
+    
+    socket.on('change-username-submit', function (data) {
+        $('#username-submit-result').text('');
+        if (data.success) {
+            $('#username-submit-result').removeClass('fail').addClass('success');
+            $.cookie('chatroom-username', data.username, {expires: 365, path: '/'});
+        } else {
+            $('#username-submit-result').removeClass('success').addClass('fail');
+        }
+        $('#username-submit-result').text(data.message);
+    });
 
-  $('#enter-message').submit(function (e) {
-    e.preventDefault();
-    var $input = $('input[name=message]');
-    socket.emit('message', {text: $input.val()});
-    $input.val('');
-  });
-  
-  $('#change-username').submit(function (e) {
-    e.preventDefault();
-    var $input = $('input[name=username]');
-    socket.emit('change-username', {username: $input.val()});
-  });
-  
+    $('#enter-message').submit(function (e) {
+        e.preventDefault();
+        var $input = $('input[name=message]');
+        socket.emit('message', {text: $input.val()});
+        $input.val('');
+    });
+    
+    $('#change-username').submit(function (e) {
+        e.preventDefault();
+        var $input = $('input[name=username]');
+        socket.emit('change-username', {username: $input.val()});
+    });
+    
 });
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express')
-  , app = express.createServer()
-  , io = require('socket.io').listen(app);
-  
+    , app = express.createServer()
+    , io = require('socket.io').listen(app);
+    
 var port = process.env.PORT || 8000;
 
 app.listen(port);
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/templates/index.html');
+    res.sendfile(__dirname + '/templates/index.html');
 });
 
 app.use(express.static(__dirname + '/public'));
@@ -20,98 +20,98 @@ var users = {};
 var currentUsernames = [];
 
 var generateGuestUsername = function () {
-  var randomNumber = Math.floor(Math.random()*maxUsers);
-  var username = 'guest' + randomNumber;
-  if (currentUsernames.indexOf(username) == -1) {
-    return username;
-  } else {
-    return generateGuestUsername();
-  }
+    var randomNumber = Math.floor(Math.random()*maxUsers);
+    var username = 'guest' + randomNumber;
+    if (currentUsernames.indexOf(username) == -1) {
+        return username;
+    } else {
+        return generateGuestUsername();
+    }
 };
 
 var stripUsername = function (username) {
-  return username.replace(/(^\s*)|(\s*$)/g, '');
+    return username.replace(/(^\s*)|(\s*$)/g, '');
 };
 
 var createUser = function (id, username) {
-  if (typeof username == 'string') {
-    username = stripUsername(username);
-    if (currentUsernames.indexOf(username) != -1) {
-      username = null;
+    if (typeof username == 'string') {
+        username = stripUsername(username);
+        if (currentUsernames.indexOf(username) != -1) {
+            username = null;
+        }
     }
-  }
-  
-  if (!username) {
-    // Create username guestxxxx to start with
-    username = generateGuestUsername();
-  }
-  
-  users[id] = username;
-  currentUsernames.push(username);
-  return username;
+    
+    if (!username) {
+        // Create username guestxxxx to start with
+        username = generateGuestUsername();
+    }
+    
+    users[id] = username;
+    currentUsernames.push(username);
+    return username;
 };
 
 var disconnectUser = function (id) {
-  var username = users[id];
-  var userIndex = currentUsernames.indexOf(username);
-  if (userIndex != -1) {
-    currentUsernames.splice(userIndex, 1);
-  }
-  delete users[id];
-  return username;
+    var username = users[id];
+    var userIndex = currentUsernames.indexOf(username);
+    if (userIndex != -1) {
+        currentUsernames.splice(userIndex, 1);
+    }
+    delete users[id];
+    return username;
 };
 
 io.sockets.on('connection', function (socket) {
-  if (currentUsernames.length < maxUsers) {
-    socket.emit('welcome');
-  
-    socket.on('join', function (data) {
-      if (data.lastLoad < lastRestart) {
-        socket.emit('reload'); // Force users to reload page if server restarted
-      } else {
-        var username = createUser(socket.id, data.username);
-        if (username) {
-          socket.broadcast.emit('joined', {username: username, id: socket.id});
-          socket.emit('joined', {username: username, self: true, id: socket.id});
-          socket.emit('userlist', {users: users});
-          
-          socket.on('message', function (data) {
-            var username = users[socket.id];
-            socket.broadcast.emit('message', {text: data.text, username: username});
-            socket.emit('message', {text: data.text, username: username});
-          });
-          
-          socket.on('change-username', function (data) {
-            var username = stripUsername(data.username);
-            if (username.length > usernameMaxLength) {
-              socket.emit('change-username-submit', {success: false, message: 'Username must be less than ' + usernameMaxLength + ' characters.'});
-            } else if (!username) {
-              socket.emit('change-username-submit', {success: false, message: 'Username cannot be blank.'});
+    if (currentUsernames.length < maxUsers) {
+        socket.emit('welcome');
+    
+        socket.on('join', function (data) {
+            if (data.lastLoad < lastRestart) {
+                socket.emit('reload'); // Force users to reload page if server restarted
             } else {
-              if (currentUsernames.indexOf(username) == -1) {
-                var oldUsername = users[socket.id];
-                var index = currentUsernames.indexOf(oldUsername);
-                users[socket.id] = username;
-                currentUsernames.splice(index, 1);
-                currentUsernames.push(username);
-                
-                socket.broadcast.emit('changed-username', {id: socket.id, oldName: oldUsername, newName: username});
-                socket.emit('changed-username', {id: socket.id, oldName: oldUsername, newName: username});
-                socket.emit('change-username-submit', {success: true, message: 'Username successfully changed.', username: username});
-              } else {
-                socket.emit('change-username-submit', {success: false, message: 'Username already in use; please try another.'});
-              }
+                var username = createUser(socket.id, data.username);
+                if (username) {
+                    socket.broadcast.emit('joined', {username: username, id: socket.id});
+                    socket.emit('joined', {username: username, self: true, id: socket.id});
+                    socket.emit('userlist', {users: users});
+                    
+                    socket.on('message', function (data) {
+                        var username = users[socket.id];
+                        socket.broadcast.emit('message', {text: data.text, username: username});
+                        socket.emit('message', {text: data.text, username: username});
+                    });
+                    
+                    socket.on('change-username', function (data) {
+                        var username = stripUsername(data.username);
+                        if (username.length > usernameMaxLength) {
+                            socket.emit('change-username-submit', {success: false, message: 'Username must be less than ' + usernameMaxLength + ' characters.'});
+                        } else if (!username) {
+                            socket.emit('change-username-submit', {success: false, message: 'Username cannot be blank.'});
+                        } else {
+                            if (currentUsernames.indexOf(username) == -1) {
+                                var oldUsername = users[socket.id];
+                                var index = currentUsernames.indexOf(oldUsername);
+                                users[socket.id] = username;
+                                currentUsernames.splice(index, 1);
+                                currentUsernames.push(username);
+                                
+                                socket.broadcast.emit('changed-username', {id: socket.id, oldName: oldUsername, newName: username});
+                                socket.emit('changed-username', {id: socket.id, oldName: oldUsername, newName: username});
+                                socket.emit('change-username-submit', {success: true, message: 'Username successfully changed.', username: username});
+                            } else {
+                                socket.emit('change-username-submit', {success: false, message: 'Username already in use; please try another.'});
+                            }
+                        }
+                    });
+                    
+                    socket.on('disconnect', function (data) {
+                        var username = disconnectUser(socket.id);
+                        socket.broadcast.emit('left', {username: username, id: socket.id});
+                    });
+                }
             }
-          });
-          
-          socket.on('disconnect', function (data) {
-            var username = disconnectUser(socket.id);
-            socket.broadcast.emit('left', {username: username, id: socket.id});
-          });
-        }
-      }
-    });
-  } else {
-    socket.emit('server-full');
-  }
+        });
+    } else {
+        socket.emit('server-full');
+    }
 });

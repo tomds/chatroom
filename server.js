24b37afbf89b8c156934d9230579fd540d9cbecc
Tom de Simone <mail@tomandpete.co.uk>
2012-05-11 22:08:11 +0100

    code reshuffling

--- server.js
+++ server.js
@@ -19,6 +19,65 @@ var usernameMaxLength = 30;
 var users = {};
 var currentUsernames = [];
 
+io.sockets.on('connection', function (socket) {
+    if (currentUsernames.length < maxUsers) {
+        socket.emit('welcome');
+    
+        socket.on('join', function (data) {
+            if (data.lastLoad < lastRestart) {
+                socket.emit('reload'); // Force users to reload page if server restarted
+            } else {
+                var username = createUser(socket.id, data.username);
+                if (username) {
+                    socket.broadcast.emit('joined', {username: username, id: socket.id});
+                    socket.emit('joined', {username: username, self: true, id: socket.id});
+                    socket.emit('userlist', {users: users});
+                    
+                    bindChat(socket);
+                }
+            }
+        });
+    } else {
+        socket.emit('server-full');
+    }
+});
+
+var bindChat = function (socket) {
+    socket.on('message', function (data) {
+        var username = users[socket.id];
+        socket.broadcast.emit('message', {text: data.text, username: username});
+        socket.emit('message', {text: data.text, username: username});
+    });
+    
+    socket.on('change-username', function (data) {
+        var username = stripUsername(data.username);
+        if (username.length > usernameMaxLength) {
+            socket.emit('change-username-submit', {success: false, message: 'Username must be less than ' + usernameMaxLength + ' characters.'});
+        } else if (!username) {
+            socket.emit('change-username-submit', {success: false, message: 'Username cannot be blank.'});
+        } else {
+            if (currentUsernames.indexOf(username) == -1) {
+                var oldUsername = users[socket.id];
+                var index = currentUsernames.indexOf(oldUsername);
+                users[socket.id] = username;
+                currentUsernames.splice(index, 1);
+                currentUsernames.push(username);
+                
+                socket.broadcast.emit('changed-username', {id: socket.id, oldName: oldUsername, newName: username});
+                socket.emit('changed-username', {id: socket.id, oldName: oldUsername, newName: username});
+                socket.emit('change-username-submit', {success: true, message: 'Username successfully changed.', username: username});
+            } else {
+                socket.emit('change-username-submit', {success: false, message: 'Username already in use; please try another.'});
+            }
+        }
+    });
+    
+    socket.on('disconnect', function (data) {
+        var username = disconnectUser(socket.id);
+        socket.broadcast.emit('left', {username: username, id: socket.id});
+    });
+};
+
 var generateGuestUsername = function () {
     var randomNumber = Math.floor(Math.random()*maxUsers);
     var username = 'guest' + randomNumber;
@@ -60,58 +119,3 @@ var disconnectUser = function (id) {
     delete users[id];
     return username;
 };
-
-io.sockets.on('connection', function (socket) {
-    if (currentUsernames.length < maxUsers) {
-        socket.emit('welcome');
-    
-        socket.on('join', function (data) {
-            if (data.lastLoad < lastRestart) {
-                socket.emit('reload'); // Force users to reload page if server restarted
-            } else {
-                var username = createUser(socket.id, data.username);
-                if (username) {
-                    socket.broadcast.emit('joined', {username: username, id: socket.id});
-                    socket.emit('joined', {username: username, self: true, id: socket.id});
-                    socket.emit('userlist', {users: users});
-                    
-                    socket.on('message', function (data) {
-                        var username = users[socket.id];
-                        socket.broadcast.emit('message', {text: data.text, username: username});
-                        socket.emit('message', {text: data.text, username: username});
-                    });
-                    
-                    socket.on('change-username', function (data) {
-                        var username = stripUsername(data.username);
-                        if (username.length > usernameMaxLength) {
-                            socket.emit('change-username-submit', {success: false, message: 'Username must be less than ' + usernameMaxLength + ' characters.'});
-                        } else if (!username) {
-                            socket.emit('change-username-submit', {success: false, message: 'Username cannot be blank.'});
-                        } else {
-                            if (currentUsernames.indexOf(username) == -1) {
-                                var oldUsername = users[socket.id];
-                                var index = currentUsernames.indexOf(oldUsername);
-                                users[socket.id] = username;
-                                currentUsernames.splice(index, 1);
-                                currentUsernames.push(username);
-                                
-                                socket.broadcast.emit('changed-username', {id: socket.id, oldName: oldUsername, newName: username});
-                                socket.emit('changed-username', {id: socket.id, oldName: oldUsername, newName: username});
-                                socket.emit('change-username-submit', {success: true, message: 'Username successfully changed.', username: username});
-                            } else {
-                                socket.emit('change-username-submit', {success: false, message: 'Username already in use; please try another.'});
-                            }
-                        }
-                    });
-                    
-                    socket.on('disconnect', function (data) {
-                        var username = disconnectUser(socket.id);
-                        socket.broadcast.emit('left', {username: username, id: socket.id});
-                    });
-                }
-            }
-        });
-    } else {
-        socket.emit('server-full');
-    }
-});
